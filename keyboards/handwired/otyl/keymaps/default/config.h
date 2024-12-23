858d1ab3a4cfe454f1db6e9e7a7196fe36b1886a
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2024-12-23 21:34:10 +0100

    feat: add otyl

---
+++ keyboards/handwired/otyl/keymaps/default/config.h
@@ -0,0 +1,30 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* Select hand configuration */
+#define EE_HANDS
+
+/* Keyboard matrix assignments */
+#define MATRIX_ROW_PINS \
+    { GP6, GP7, GP8, GP9, GP10 }
+#define MATRIX_COL_PINS \
+    { GP22, GP21, GP20, GP19, GP18, GP17 }
