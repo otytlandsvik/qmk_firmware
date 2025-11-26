2557eae4b1c9529e783a5f7de823b83295c64437
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2025-11-26 19:55:27 +0100

    feat: F/vim layer

--- keyboards/handwired/otyl/keymaps/default/keymap.c
+++ keyboards/handwired/otyl/keymaps/default/keymap.c
@@ -5,6 +5,7 @@
 #define GUI_ESC RGUI_T(KC_ESC)
 #define MENT LT(2, KC_ENT)
 #define SFT_BSP LSFT_T(KC_BSPC)
+#define ALT_Z LALT_T(KC_Z)
 
 /* Æ, Ø and Å */
 #define KC_AE RALT(KC_Z)
@@ -32,26 +33,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                             +-------------+------+               +-------------+------+
      */
     [0] = LAYOUT(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
+        MO(3),   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_TAB,  ALT_Z,   KC_X,    KC_C,    KC_V,    KC_B,                   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
                           KC_LBRC, KC_RBRC,                                                    KC_PLUS, KC_EQL,
-                                   MSPC,    GUI_ESC, KC_LCTL,                KC_LALT, SFT_BSP, MENT
+                                   MSPC,    GUI_ESC, MO(3),                  KC_LCTL, SFT_BSP, MENT
     ),
 
     [1] = LAYOUT(
-        _______, _______, KC_AMPR, KC_ASTR, _______, _______,                KC_AE,   KC_OE,   KC_AA,   KC_LPRN, KC_RPRN, _______,
+        QK_BOOT, _______, KC_AMPR, KC_ASTR, _______, _______,                KC_AE,   KC_OE,   KC_AA,   KC_LPRN, KC_RPRN, _______,
         _______, _______, KC_DLR,  KC_PERC, KC_CIRC, KC_TILD,                KC_LT,   KC_PIPE, KC_GT,   KC_LCBR, KC_RCBR, _______,
         _______, _______, KC_EXLM, KC_AT,   KC_HASH, KC_GRV,                 KC_EQL,  KC_MINS, KC_PLUS, KC_LBRC, KC_RBRC, _______,
+                          _______, _______,                                                    _______, _______,
+                                   _______, _______, _______,                _______, _______, MO(3)
+    ),
+
+    [2] = LAYOUT(
+        _______, _______, KC_7,    KC_8,    KC_9,    KC_0,                   KC_PGDN, KC_PGUP, KC_HOME, KC_END,  _______, QK_BOOT,
+        _______, _______, KC_4,    KC_5,    KC_6,    KC_DOT,                 KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+        _______, _______, KC_1,    KC_2,    KC_3,    KC_COMMA,               PPAGE,   PTAB,    NTAB,    NPAGE,   _______, _______,
                           _______, _______,                                                    _______, _______,
                                    _______, _______, _______,                _______, _______, _______
     ),
 
-    [2] = LAYOUT(
-        _______, _______, KC_7,    KC_8,    KC_9, _______,                   KC_PGDN, KC_PGUP, KC_HOME, KC_END,  _______, _______,
-        _______, _______, KC_4,    KC_5,    KC_6, KC_DOT,                    KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, _______, _______,
-        _______, _______, KC_1,    KC_2,    KC_3, KC_COMMA,                  PPAGE,   PTAB,    NTAB,    NPAGE,   _______, _______,
+    [3] = LAYOUT(
+        _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,                 _______, _______, _______, _______, _______, _______,
+        _______, _______, KC_F6,   KC_F5,   KC_F6,   KC_F11,                 LCTL(KC_H), LCTL(KC_J), LCTL(KC_K), LCTL(KC_L), _______, _______,
+        _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F12,                 LSFT(KC_H), LSFT(KC_J), LSFT(KC_K), LSFT(KC_L), _______, _______,
                           _______, _______,                                                    _______, _______,
-                                   _______, _______, _______,                _______, _______, _______
+                                   _______, _______, _______,                _______, KC_PSCR,  _______
     )
 };
