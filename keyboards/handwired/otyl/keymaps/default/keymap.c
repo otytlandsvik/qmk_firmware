448f8c51a53b8be9223a2283fc4c135c0dffedb7
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2025-11-26 20:24:26 +0100

    feat: initial graphite implementation

--- keyboards/handwired/otyl/keymaps/default/keymap.c
+++ keyboards/handwired/otyl/keymaps/default/keymap.c
@@ -5,7 +5,7 @@
 #define GUI_ESC RGUI_T(KC_ESC)
 #define MENT LT(2, KC_ENT)
 #define SFT_BSP LSFT_T(KC_BSPC)
-#define ALT_Z LALT_T(KC_Z)
+#define ALT_Q LALT_T(KC_Q)
 
 /* Æ, Ø and Å */
 #define KC_AE RALT(KC_Z)
@@ -33,15 +33,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                             +-------------+------+               +-------------+------+
      */
     [0] = LAYOUT(
-        MO(3),   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_TAB,  ALT_Z,   KC_X,    KC_C,    KC_V,    KC_B,                   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
+        MO(3),   KC_B,    KC_L,    KC_D,    KC_W,    KC_Z,                   KC_QUOT, KC_F,    KC_O,    KC_U,    KC_J,    KC_SCLN,
+        KC_LCTL, KC_N,    KC_R,    KC_T,    KC_S,    KC_G,                   KC_Y,    KC_H,    KC_A,    KC_E,    KC_I,    KC_COMM,
+        KC_TAB,  ALT_Q,   KC_X,    KC_M,    KC_C,    KC_V,                   KC_K,    KC_P,    KC_DOT,  KC_MINS, KC_SLSH, KC_RCTL,
                           KC_LBRC, KC_RBRC,                                                    KC_PLUS, KC_EQL,
                                    MSPC,    GUI_ESC, MO(3),                  KC_LCTL, SFT_BSP, MENT
     ),
 
     [1] = LAYOUT(
-        QK_BOOT, _______, KC_AMPR, KC_ASTR, _______, _______,                KC_AE,   KC_OE,   KC_AA,   KC_LPRN, KC_RPRN, _______,
+        QK_BOOT, _______, KC_AMPR, KC_ASTR, KC_BSLS, _______,                KC_AE,   KC_OE,   KC_AA,   KC_LPRN, KC_RPRN, _______,
         _______, _______, KC_DLR,  KC_PERC, KC_CIRC, KC_TILD,                KC_LT,   KC_PIPE, KC_GT,   KC_LCBR, KC_RCBR, _______,
         _______, _______, KC_EXLM, KC_AT,   KC_HASH, KC_GRV,                 KC_EQL,  KC_MINS, KC_PLUS, KC_LBRC, KC_RBRC, _______,
                           _______, _______,                                                    _______, _______,
